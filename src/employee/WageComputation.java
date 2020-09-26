671390f222b3dd44ab605a7ea28614bdc414893f
aditya Kharade <adityakharade51@gmail.com>
2020-09-26 22:35:19 +0530

    Part time wage

--- src/employee/WageComputation.java
+++ src/employee/WageComputation.java
@@ -3,12 +3,16 @@ package employee;
 public class WageComputation {
 	public static void main(String[] args) {
 		int full_time = 1;
+		int part_time = 2;
 		int wage_per_hr = 20;
 		int emp_wage =0;
 		int emp_hrs =0;  
-		int empCheck = (int) Math.floor((Math.random() * 10))%2;
+		int empCheck = (int) Math.floor((Math.random() * 10))%3;
 		if(empCheck == full_time) {
 			emp_hrs = 8;
+		}
+		else if(empCheck == part_time) {
+			emp_hrs = 4;
 		}
 		emp_wage = wage_per_hr * emp_hrs;
 		System.out.println("Employee wage is : "+ emp_wage);
