3926f1625b83fbd010e2cc16f2f1c23aabe7cc69
aditya Kharade <adityakharade51@gmail.com>
2020-09-26 21:51:09 +0530

    Employee Attendance

--- src/employee/WageComputation.java
+++ src/employee/WageComputation.java
@@ -1,5 +1,14 @@
 package employee;
 
 public class WageComputation {
-
+	public static void main(String[] args) {
+		int full_time = 1;
+		int empCheck = (int) Math.floor((Math.random() * 10))%2;
+		if(empCheck == full_time) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Abscent");
+		}
+	}
 }
