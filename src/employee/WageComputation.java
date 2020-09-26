2b6c74879ea8ad96ec29cca31d106c490336550c
aditya Kharade <adityakharade51@gmail.com>
2020-09-26 22:23:48 +0530

    Daily Employee Wage

--- src/employee/WageComputation.java
+++ src/employee/WageComputation.java
@@ -3,12 +3,14 @@ package employee;
 public class WageComputation {
 	public static void main(String[] args) {
 		int full_time = 1;
+		int wage_per_hr = 20;
+		int emp_wage =0;
+		int emp_hrs =0;  
 		int empCheck = (int) Math.floor((Math.random() * 10))%2;
 		if(empCheck == full_time) {
-			System.out.println("Employee is Present");
+			emp_hrs = 8;
 		}
-		else {
-			System.out.println("Employee is Abscent");
-		}
+		emp_wage = wage_per_hr * emp_hrs;
+		System.out.println("Employee wage is : "+ emp_wage);
 	}
 }
