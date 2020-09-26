de90a997c3fd72bd58236f9533e3dda4dfb40fa1
aditya Kharade <adityakharade51@gmail.com>
2020-09-26 23:05:43 +0530

    Calculating wages for a month

--- src/employee/WageComputation.java
+++ src/employee/WageComputation.java
@@ -1,22 +1,30 @@
 package employee;
 
 public class WageComputation {
+	public static final int full_time = 1;
+	public static final int part_time = 2;
+	public static final int wage_per_hr = 20;
+	public static final int no_working_days = 20;
 	public static void main(String[] args) {
-		int full_time = 1;
-		int part_time = 2;
-		int wage_per_hr = 20;
+		
+		
 		int emp_wage =0;
 		int emp_hrs =0;  
-		int empCheck = (int) Math.floor((Math.random() * 10))%3;
-		switch(empCheck) {
-			case 1: emp_hrs = 8;
-					break;
-			case 2: emp_hrs = 4;
-					break;
-			default: emp_hrs = 0;
-					 break;
+		int total_empwage = 0;
+		for(int day = 0; day < no_working_days; day++) {
+			int empCheck = (int) Math.floor((Math.random() * 10))%3;
+			switch(empCheck) {
+				case full_time: emp_hrs = 8;
+						break;
+				case part_time: emp_hrs = 4;
+						break;
+				default: emp_hrs = 0;
+						 break;
+			}
+		
+			emp_wage = wage_per_hr * emp_hrs;
+			total_empwage += emp_wage; 
 		}
-		emp_wage = wage_per_hr * emp_hrs;
-		System.out.println("Employee wage is : "+ emp_wage);
+		System.out.println("Total Employee wage is : "+ total_empwage);
 	}
 }
