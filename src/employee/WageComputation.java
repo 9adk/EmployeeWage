d10d2a51bf807e32a1e1ee427a0b00e756bc4b67
aditya Kharade <adityakharade51@gmail.com>
2020-09-26 23:33:19 +0530

    Conditional usecase

--- src/employee/WageComputation.java
+++ src/employee/WageComputation.java
@@ -5,13 +5,16 @@ public class WageComputation {
 	public static final int part_time = 2;
 	public static final int wage_per_hr = 20;
 	public static final int no_working_days = 20;
+	public static final int max_hrs_in_month = 100;
 	public static void main(String[] args) {
 		
-		
-		int emp_wage =0;
-		int emp_hrs =0;  
 		int total_empwage = 0;
-		for(int day = 0; day < no_working_days; day++) {
+		int total_emp_hrs = 0;
+		int total_working_days = 0;
+		while(total_emp_hrs < max_hrs_in_month 
+				&& total_working_days < no_working_days) {
+			total_working_days++;
+			int emp_hrs = 0;
 			int empCheck = (int) Math.floor((Math.random() * 10))%3;
 			switch(empCheck) {
 				case full_time: emp_hrs = 8;
@@ -22,9 +25,11 @@ public class WageComputation {
 						 break;
 			}
 		
-			emp_wage = wage_per_hr * emp_hrs;
+			total_emp_hrs += emp_hrs; 
+			int emp_wage = emp_hrs * wage_per_hr;
 			total_empwage += emp_wage; 
+			System.out.println("Employee Wage : "+ emp_wage);
+			}
+		System.out.println("Total employee wage : "+ total_empwage);
 		}
-		System.out.println("Total Employee wage is : "+ total_empwage);
-	}
 }
