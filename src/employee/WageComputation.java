6aea667a716406ea9c560a033bf161bc59cffcb9
aditya Kharade <adityakharade51@gmail.com>
2020-09-28 17:02:52 +0530

    Refactored : changed the structure of code

--- src/employee/WageComputation.java
+++ src/employee/WageComputation.java
@@ -6,20 +6,22 @@ public class WageComputation {
 	public static final int wage_per_hr = 20;
 	public static final int no_working_days = 20;
 	public static final int max_hrs_in_month = 100;
-	public static void main(String[] args) {
-		
+	
+	public static int calculateWage() {
 		int total_empwage = 0;
 		int total_emp_hrs = 0;
 		int total_working_days = 0;
 		while(total_emp_hrs < max_hrs_in_month 
 				&& total_working_days < no_working_days) {
-			total_working_days++;
+			
 			int emp_hrs = 0;
 			int empCheck = (int) Math.floor((Math.random() * 10))%3;
 			switch(empCheck) {
 				case full_time: emp_hrs = 8;
+								total_working_days++;
 						break;
 				case part_time: emp_hrs = 4;
+								total_working_days++;
 						break;
 				default: emp_hrs = 0;
 						 break;
@@ -28,8 +30,16 @@ public class WageComputation {
 			total_emp_hrs += emp_hrs; 
 			int emp_wage = emp_hrs * wage_per_hr;
 			total_empwage += emp_wage; 
-			System.out.println("Employee Wage : "+ emp_wage);
+			
+			System.out.println("Employee Wage of the particular day: "+ emp_wage);
 			}
+		return total_empwage;
+	}
+	
+	public static void main(String[] args) {
+		int total_empwage;
+		total_empwage = calculateWage();
 		System.out.println("Total employee wage : "+ total_empwage);
+
 		}
 }
